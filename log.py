9a0235179ed399ee0904d0f1e12df329d6913a40
Jacob <jacobdelzer369@gmail.com>
2025-01-31 00:05:24 -0700

    Method (WIP)

--- log.py
+++ log.py
@@ -11,26 +11,48 @@ cont = 0
 values = {
 
   'path': '/sys/class/thermal/thermal_zone0/temp',
+  'scale': 1000,
+  'method': 0,
+  'methodInfo': [],
 
-  'spf': 1,
-  'contLogLen': 20,
+  'spf': 1.0,
+  'logLen': 20.0,
 
-  'barMin': 20,
-  'barMax': 100,
-  'barLen': 50,
+  'barMin': 20.0,
+  'barMax': 100.0,
+  'barLen': 50.0,
   'barMed': .7,
   'barHi': .85,
   'barChr': '=',
-  'barLoC': 32,
-  'barMedC': 33,
-  'barHiC': 31
+  'barLoC': 32.0,
+  'barMedC': 33.0,
+  'barHiC': 31.0
   
 }
 
 # Keys
 
 types = {
-  'thermal': '/sys/class/thermal/thermal_zone0/temp'
+  'thermal': ['/sys/class/thermal/thermal_zone0/temp',
+              1000,
+              0,
+              [],
+              'CPU temp in Celcius'],
+  'cpughz': ['/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq',
+             1000000,
+             0,
+             [],
+             'CPU Clocking in GHz. "cpu0" can be interchanged for different CPU'],
+  'netrx': ['/sys/class/net/eth0/statistics/rx_bytes',
+             1048576,
+             0,
+             [],
+             'MB received. "eth0" can be interchanged for different network device'],
+  'nettx': ['/sys/class/net/eth0/statistics/tx_bytes',
+             1048576,
+             0,
+             [],
+             'MB transmitted. "eth0" can be interchanged for different network device']
 }
 
 colorKey = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
@@ -51,6 +73,37 @@ def strToFloat(string):
   
   if string[0] == '-': return -1 * float(numStr)
   else: return float(numStr)
+  
+
+def strToArray(string):
+  
+  out = []
+  
+  var = ''
+  
+  div = ' '
+  
+  string = string + div
+  
+  for i in range(len(string)):
+    
+    if string[i-len(div):i] == div:
+      
+      var = var[:-len(div)]
+      
+      out.append(var)
+      
+      var = ''
+      
+    
+    var += string[i]
+    
+  
+  var = var[:-len(div)]
+  
+  out.append(var)
+  
+  return out
   
 
 def lenNum(string, length): # Adds 0's to string until at length
@@ -90,8 +143,8 @@ def bar(val, minimum, maximum, length, medium, high, char, loColor, medColor, hi
   
   for i in range(barLen):
     
-    if i / length >= high: out = out + '[\033[' + str(hiColor) + 'm'
-    elif i / length >= medium: out = out + '[\033[' + str(medColor) + 'm'
+    if i / length >= high: out = out + '\033[' + str(hiColor) + 'm'
+    elif i / length >= medium: out = out + '\033[' + str(medColor) + 'm'
     
     out = out + char
     
@@ -120,26 +173,39 @@ def printLog(log, new):
   return log
   
 
-# Try File
-
-try:
-  with open(values['path'], 'r') as file: pass
-except:
-  print('Unable to Open :/\n')
-  run = False
+def getCont(path, method, methodInfo):
+  
+  out = 0
+  
+  if method == 0:
+    
+    with open(path, 'r') as file: out = strToFloat(file.read())
+    
+  
+  return out / values['scale']
 
 # Instructions
 
 print('Inputs:')
 print('  Case does not matter')
-print('  Numbers are validated before use (if logLen is set to -1.4 it will be treated as -1)')
+print('  Numbers are validated before use')
+print('    (if logLen is set to -5.2 it will be treated as 1)')
 print()
 print('  "quit": Quits')
-print('  "run": run graph loop (must kill program to stop)')
-print('  "spf": seconds per frame for graph, Default: 1')
+print('  "run": Run graph loop (must kill program to stop)')
+print('  "spf": Seconds per frame for graph, Default: 1')
 print('  "logLen": How many lines are recorded, Default: 20')
 print()
 print('  "path": File path for data file, Defualt: (for thermal)')
+print('  "scale": Scale of return value, Default: 1000')
+print('  "method": Method for gathering info, Default: 0')
+print('    0: Raw')
+print('    1: Inverse, (total - raw) / total')
+print('    2: Divide by time, (new - old) / "spf"')
+print('  "methodInfo": Other info needed for gathering data, Default: []')
+print('    0: []')
+print('    1: [totalName, rawName]')
+print('    2: [old]')
 print('  "type": Looks up paths saved in list for data file')
 print('  "type?": Print types in array noted above')
 print()
@@ -153,6 +219,14 @@ print('  "barLoC": Low color, Default: 32 (green)')
 print('  "barMedC": Medium color, Default: 33 (yellow)')
 print('  "barHiC": High color, Default: 31 (red)')
 print('  "c?": Print color key')
+
+# Try File
+
+try:
+  with open(values['path'], 'r') as file: pass
+except:
+  print('\nUnable to Open :/')
+  run = False
 
 # Input Loop
 
@@ -180,16 +254,44 @@ while True:
   # Info
   
   elif inp == 'type?':
-    for key in types: print(key + ': ' + types[key])
+    for key in types: print(key + ': ' + types[key][0] +
+                            ', scale: ' + str(types[key][1]) +
+                            '\n  ' + types[key][2])
   
   elif inp == 'c?':
-    for i in range(len(colorKey)): print(colorKey[i] + ': ' + str(i))
+    for i in range(len(colorKey)): print(colorKey[i] + ': ' + str(i+31))
+  
+  # Type
+  
+  elif inp == 'type':
+    
+    valInp = input('"type": ')
+    print()
+    
+    for key in types:
+      
+      if valInp == key:
+        
+        try:
+          with open(types[key][0], 'r') as file: pass
+        except:
+          print('Unable to Open :/')
+          run = False
+        else:
+          values['path'] = types[key][0]
+          print('"path" set to "' + values['path'] + '"')
+          
+          values['scale'] = types[key][1]
+          print('"scale" set to "' + str(values['scale']) + '"')
+        
+      
+    
   
   # String Input
   
   elif inp == 'path':
     
-    valInp = input('path: ')
+    valInp = input('"path": ')
     print()
     
     try:
@@ -198,18 +300,28 @@ while True:
       print('Unable to Open :/')
       run = False
     else:
-      values['path'] = pathInp
+      values['path'] = valInp
       print('"path" set to "' + values['path'] + '"')
     
   
-  elif inp == 'barChr':
+  elif inp == 'barchr':
     
-    valInp = input('Character: ')
+    valInp = input('"barChr": ')
     print()
     
     values['barChr'] = (valInp + ' ')[0]
-    
     print('"barChr" set to "' + values['barChr'] + '"')
+    
+  
+  # Method Info / Array Input
+  
+  elif inp == "methodinfo":
+    
+    valInp = input('"methodInp: ')
+    print()
+    
+    values['methodInfo'] = strToArray(valInp)
+    print('"methodInfo" set to "' + values['methodInfo'] + '"')
     
   
   # All Other
@@ -218,21 +330,21 @@ while True:
     
     for key in values:
       
-      if inp == key:
+      if inp == key.lower():
         
         valInp = input('"' + key + '": ')
         print()
         
         values[key] = strToFloat(valInp)
         
-        print('"' + key +'" set to "' + values[key] + '"')
+        print('"' + key +'" set to "' + str(values[key]) + '"')
         
     
   
 
 # Preloop
 
-contLog = [''] * int(max(values['contLogLen'], 0))
+contLog = [''] * int(max(values['logLen'], 1))
 
 print('Press [CTRL] + [C] to stop')
 
@@ -244,17 +356,26 @@ while run:
   
   # Read
   
-  with open(values['path'], 'r') as file: cont = strToFloat(file.read())
+  cont = getCont(values['path'], values['method'], values['methodInfo'])
   
   # Print
   
-  newLog = bar(cont/1000, barMin, barMax, barLen, barMed, barHi)
+  newLog = bar(cont, 
+               values['barMin'],
+               values['barMax'],
+               values['barLen'],
+               values['barMed'],
+               values['barHi'],
+               values['barChr'],
+               values['barLoC'],
+               values['barMedC'],
+               values['barHiC'])
   
-  newLog = newLog + ' | ' + lenNum(str(cont / 1000), 5 ) + '  '
+  newLog = newLog + ' | ' + lenNum(str(cont), 5) + '  '
   
   contLog = printLog(contLog, newLog)
   
   # Time
   
-  time.sleep(spf)
+  time.sleep(max(values['spf'], 0))
   
